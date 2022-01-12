95c606e5c8c10a50d0fcea321e484c971920c1f9
lbult <lennart.bult10@gmail.com>
2022-01-12 13:45:41 +0100

    Delete

--- support_func.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from numpy import nan, inf
-import numpy as np
-
-def clean_dataset(df):
-    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
-    df.dropna(inplace=True)
-    indices_to_keep = ~df.isin([nan, inf, inf]).any(1)
-    return df[indices_to_keep].astype(np.float64)
